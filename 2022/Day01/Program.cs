5550d73a17f206035f074a946e84299c29f03856
VeQox <sonnleitner.luki@gmail.com>
2022-12-01 19:43:29 +0100

    Day01

---
+++ 2022/Day01/Program.cs
@@ -0,0 +1,45 @@
+﻿class Program{
+  const string INPUT_PATH = "input.txt";
+  static void Main() {
+    Console.WriteLine($"PartOne: {PartOne(getInput())}");
+    Console.WriteLine($"PartTwo: {PartTwo(getInput())}");
+  }
+
+  static int PartTwo(string[] input){
+    List<int> calories = new();
+
+    int elfSum = 0;
+
+    foreach(string line in input){
+      if(line != ""){
+        elfSum += int.Parse(line);
+      }
+      else{
+        calories.Add(elfSum);
+        elfSum = 0;
+      }
+    }
+
+    return calories.AsEnumerable().OrderByDescending(calorie => calorie).Take(3).Sum();
+  }
+
+  static int PartOne(string[] input){
+    int maxCalories = 0;
+    int elfSum = 0;
+
+    foreach(string line in input){
+      if(line != ""){
+        elfSum += int.Parse(line);
+      }
+      else{
+        maxCalories = elfSum > maxCalories ? elfSum : maxCalories;
+        elfSum = 0;
+      }
+    }
+
+    return maxCalories;
+  }
+
+  static string[] getInput()
+    => File.ReadAllLines(INPUT_PATH);
+}
